d31e4c6b700602f0a4dda1bb1b8ac411346e6ba7
ysatynska <ysatynska@mail.roanoke.edu>
2025-01-16 20:55:37 -0500

    definitions

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -5,9 +5,32 @@ export type User = {
     password: string;
 };
 
-export type User = {
+export type Student = {
     id: string;
     name: string;
-    email: string;
-    password: string;
+    age: string;
+    grade: string;
+    address: string;
+    sex: string;
+    phone_number: string;
+};
+
+export type Section = {
+    id: string;
+    name: string;
+    description: string;
+    total_score: string;
+};
+
+export type StudentSectionCourseMap = {
+    section_id: string;
+    student_id: string;
+    course_id: string;
+};
+
+export type Course = {
+    id: string;
+    name: string;
+    teacher: string;
+    location: string;
 };
