148ba1976a7e0949758673a86eccec82a92872e7
ysatynska <ysatynska@mail.roanoke.edu>
2025-01-21 09:57:43 -0500

    vercel

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -5,21 +5,21 @@ import { useTheme } from "next-themes";
 import { Switch } from "@heroui/react";
 
 export default function ThemeSwitcher() {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
+  // const { theme, setTheme } = useTheme();
+  // const [mounted, setMounted] = useState(false);
 
-  // Ensure the component is mounted before accessing the theme
-  useEffect(() => setMounted(true), []);
+  // // Ensure the component is mounted before accessing the theme
+  // useEffect(() => setMounted(true), []);
 
-  if (!mounted) return null;
+  // if (!mounted) return null;
 
   return (
     <div className="flex items-center space-x-2">
-      <Switch
+      {/* <Switch
         onChange={() => setTheme(theme === "light" ? "dark" : "light")}
         size="lg"
         className="text-blue-500"
-      />
+      /> */}
     </div>
   );
 }
