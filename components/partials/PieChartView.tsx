4ff593cd0b3f632e805142f5a4b8b9e10d9dc4c4
ysatynska <ysatynska@mail.roanoke.edu>
2025-02-13 13:24:07 -0500

    vercel

--- components/partials/PieChartView.tsx
+++ components/partials/PieChartView.tsx
@@ -3,7 +3,7 @@ import DonutChart from "@/components/donut-chart";
 export default async function PieChartView () {
   return (
     <div className="w-96 h-96 mx-auto">
-      <DonutChart labels={["One", "Two", "Three"]} chartData={[3, 5, 10]} colors={["rgba(255, 206, 86, 0.6)", "rgba(75, 192, 192, 0.6)", "rgba(153, 102, 255, 0.6)"]} />
+      {/* <DonutChart labels={["One", "Two", "Three"]} chartData={[3, 5, 10]} colors={["rgba(255, 206, 86, 0.6)", "rgba(75, 192, 192, 0.6)", "rgba(153, 102, 255, 0.6)"]} /> */}
     </div>
   )
 }
